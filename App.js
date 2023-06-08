f85444a169ce956751ed397398ae4b683c9fcdf6
Gabrielrol <grfraposo@gmail.com>
2023-06-08 20:50:55 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from '@expo/vector-icons'; 
+import HomeScreen from './src/Pages/Home';
+import OptionsScreen from './src/Pages/Options';
+import SettingsScreen from './src/Pages/Settings';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tab = createBottomTabNavigator();
+
+ export default function App() {
+    return(
+      <NavigationContainer>
+        <Tab.Navigator
+          tabBarOptions={{
+           style: {
+            backgroundColor: 'black',
+          },
+        }}
+      >
+          <Tab.Screen 
+            name="Home" 
+            component={HomeScreen}
+            options={{
+            tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+        
+        <Tab.Screen
+          name="Options" 
+          component={OptionsScreen}
+          options={{
+          tabBarIcon: ({ color, size }) => (
+          <Ionicons name="options" color={color} size={size} />
+          ),
+        }}
+        />
+      <Tab.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{
+          tabBarIcon: ({ color, size }) => (
+         <Ionicons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
+
+        </Tab.Navigator>
+      </NavigationContainer>
+    )
+ }
+
+
